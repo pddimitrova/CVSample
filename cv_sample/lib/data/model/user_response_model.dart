a14bb799370787a149a44e46adebca274cb2990c
Plamena Dimitrova <plamena.dimitrova@lykke-business.ch>
2020-07-12 11:54:44 +0300

    Add UserResponseModel

---
+++ cv_sample/lib/data/model/user_response_model.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/foundation.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'user_response_model.g.dart';
+
+@JsonSerializable()
+class UserResponseModel {
+  UserResponseModel({
+    @required this.name,
+    @required this.phoneNumber,
+    @required this.email,
+    @required this.linkedInUrl,
+    @required this.profileImageUrl,
+    @required this.profileDescription,
+    @required this.education,
+    @required this.experience,
+    @required this.techSkills,
+    @required this.softSkills,
+  });
+
+  factory UserResponseModel.fromJson(Map<String, dynamic> json) =>
+      _$UserResponseModelFromJson(json);
+
+  final String name;
+  final String phoneNumber;
+  final String email;
+  final String linkedInUrl;
+  final String profileImageUrl;
+  final String profileDescription;
+  final List<Education> education;
+  final List<Experience> experience;
+  final List<String> techSkills;
+  final List<String> softSkills;
+}
+
+@JsonSerializable()
+class Education {
+  Education({
+    @required this.school,
+    @required this.degree,
+    @required this.yearStart,
+    @required this.yearEnd,
+  });
+
+  factory Education.fromJson(Map<String, dynamic> json) =>
+      _$EducationFromJson(json);
+
+  final String school;
+  final String degree;
+  final String yearStart;
+  final String yearEnd;
+}
+
+@JsonSerializable()
+class Experience {
+  Experience({
+    @required this.company,
+    @required this.position,
+    @required this.location,
+    @required this.description,
+    @required this.dateStart,
+    @required this.dateEnd,
+  });
+
+  factory Experience.fromJson(Map<String, dynamic> json) =>
+      _$ExperienceFromJson(json);
+
+  final String company;
+  final String position;
+  final String location;
+  final String description;
+  final String dateStart;
+  final String dateEnd;
+}
---
+++ cv_sample/lib/data/model/user_response_model.g.dart
@@ -0,0 +1,79 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_response_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+UserResponseModel _$UserResponseModelFromJson(Map<String, dynamic> json) {
+  return UserResponseModel(
+    name: json['Name'] as String,
+    phoneNumber: json['PhoneNumber'] as String,
+    email: json['Email'] as String,
+    linkedInUrl: json['LinkedInUrl'] as String,
+    profileImageUrl: json['ProfileImageUrl'] as String,
+    profileDescription: json['ProfileDescription'] as String,
+    education: (json['Education'] as List)
+        ?.map((e) =>
+            e == null ? null : Education.fromJson(e as Map<String, dynamic>))
+        ?.toList(),
+    experience: (json['Experience'] as List)
+        ?.map((e) =>
+            e == null ? null : Experience.fromJson(e as Map<String, dynamic>))
+        ?.toList(),
+    techSkills: (json['TechSkills'] as List)?.map((e) => e as String)?.toList(),
+    softSkills: (json['SoftSkills'] as List)?.map((e) => e as String)?.toList(),
+  );
+}
+
+Map<String, dynamic> _$UserResponseModelToJson(UserResponseModel instance) =>
+    <String, dynamic>{
+      'Name': instance.name,
+      'PhoneNumber': instance.phoneNumber,
+      'Email': instance.email,
+      'LinkedInUrl': instance.linkedInUrl,
+      'ProfileImageUrl': instance.profileImageUrl,
+      'ProfileDescription': instance.profileDescription,
+      'Education': instance.education,
+      'Experience': instance.experience,
+      'TechSkills': instance.techSkills,
+      'SoftSkills': instance.softSkills,
+    };
+
+Education _$EducationFromJson(Map<String, dynamic> json) {
+  return Education(
+    school: json['School'] as String,
+    degree: json['Degree'] as String,
+    yearStart: json['YearStart'] as String,
+    yearEnd: json['YearEnd'] as String,
+  );
+}
+
+Map<String, dynamic> _$EducationToJson(Education instance) => <String, dynamic>{
+      'School': instance.school,
+      'Degree': instance.degree,
+      'YearStart': instance.yearStart,
+      'YearEnd': instance.yearEnd,
+    };
+
+Experience _$ExperienceFromJson(Map<String, dynamic> json) {
+  return Experience(
+    company: json['Company'] as String,
+    position: json['Position'] as String,
+    location: json['Location'] as String,
+    description: json['Description'] as String,
+    dateStart: json['DateStart'] as String,
+    dateEnd: json['DateEnd'] as String,
+  );
+}
+
+Map<String, dynamic> _$ExperienceToJson(Experience instance) =>
+    <String, dynamic>{
+      'Company': instance.company,
+      'Position': instance.position,
+      'Location': instance.location,
+      'Description': instance.description,
+      'DateStart': instance.dateStart,
+      'DateEnd': instance.dateEnd,
+    };
